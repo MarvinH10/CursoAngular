32f07775bb03a475d5d01476a9d58ec038cc37df
MarvinH10 <marvinhectorcamposdeza@gmail.com>
2024-11-17 22:51:01 -0500

    Increment and Decrement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public title: string = 'Hola Mundo';
   public counter: number = 10;
+
+  increaseBy( value: number ):void {
+    this.counter += value
+  }
+
+  decreaseBy( value: number ):void {
+    this.counter -= value
+  }
 }
