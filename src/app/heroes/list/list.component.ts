8fc38ac3dc7e9677bb9aee8064cf9db3ec60c22f
MarvinH10 <marvinhectorcamposdeza@gmail.com>
2024-11-22 10:24:23 -0500

    Learning

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+  public heroNames: string[] = ['Spidermnan', 'Ironman', 'Hulk', 'Thor'];
+  public deleteHero?: string;
 
+  removeLastHero(): void {
+    this.deleteHero = this.heroNames.pop()
+  }
 }
