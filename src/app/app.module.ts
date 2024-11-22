6d9b8d128aff127f9fef90b4366eeb2dfe4ab461
MarvinH10 <marvinhectorcamposdeza@gmail.com>
2024-11-22 15:01:03 -0500

    Aprendiendo Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HeroesModule } from './heroes/heroes.module';
 import { NgModule } from '@angular/core';
 
 import { BrowserModule } from '@angular/platform-browser';
@@ -5,19 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { CounterModule } from './counter/components/counter.module';
 
 import { AppComponent } from './app.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroComponent,
-    ListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CounterModule,
+    HeroesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
